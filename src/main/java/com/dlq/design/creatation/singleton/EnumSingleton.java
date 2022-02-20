d671b79de4bc7737e3bcc8e66c790908bfe2cd84
DLQ <1114626450@qq.com>
2022-02-20 22:51:41 +0800

    饿汉式单例 - 枚举方式

---
+++ src/main/java/com/dlq/design/creatation/singleton/EnumSingleton.java
@@ -0,0 +1,11 @@
+package com.dlq.design.creatation.singleton;
+
+/**
+ *@program: design-patterns
+ *@description: 饿汉式单例 - 枚举方式
+ *@author: Hasee
+ *@create: 2022-02-20 22:35
+ */
+public enum EnumSingleton {
+    INSTANCE;
+}
