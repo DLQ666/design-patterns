4c8bf036f0be799dffdc4dcbab613ccf207169b0
DLQ <1114626450@qq.com>
2022-02-20 22:49:22 +0800

    初始化工程

---
+++ src/main/java/com/dlq/design/creatation/singleton/MainTest.java
@@ -0,0 +1,13 @@
+package com.dlq.design.creatation.singleton;
+
+/**
+ *@program: design-patterns
+ *@description:
+ *@author: Hasee
+ *@create: 2022-02-20 22:26
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+    }
+}
