2a2fd05be6fef71f89321ce407aae2dd52bc2a6f
DLQ <1114626450@qq.com>
2022-07-27 23:44:14 +0800

    外观模式 - 外观模式在MyBatis框架应用的源码分析

---
+++ src/main/java/com/dlq/design/structural/facade/MybatisFacade.java
@@ -0,0 +1,16 @@
+package com.dlq.design.structural.facade;
+
+import org.apache.ibatis.session.Configuration;
+
+/**
+ *@program: design-patterns
+ *@description:
+ *@author: Hasee
+ *@create: 2022-07-27 22:59
+ */
+public class MybatisFacade {
+
+    public static void main(String[] args) {
+        //Configuration
+    }
+}
